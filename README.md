60f2c4b112a710900b1523ca6e1cf2b665a738eb
humonnom <juepark42@gmail.com>
2024-09-21 19:33:35 +0900

    gradient text completed

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Tailwind CSS 실습
 
 * Gradient icons ✅
-* Text background gradients, text color gradients
+* Text background gradients, text color gradients ✅
 * Animated buttons
 * Custom selector (Next ui)
 * Modal components (semantic)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import GradientIcon from "@/components/GradientIcon";
+import GradientText from "@/components/GradientText";
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
-        <GradientIcon />
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+    >
+      <GradientIcon />
+      <GradientText />
     </main>
-  )
+  );
 }
