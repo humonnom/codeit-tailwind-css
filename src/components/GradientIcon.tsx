8fc79d532f8696a3905a835cdf5d6fd5e17a691e
humonnom <juepark42@gmail.com>
2024-09-22 00:45:01 +0900

    fix from color

--- src/components/GradientIcon.tsx
+++ src/components/GradientIcon.tsx
@@ -1,6 +1,7 @@
 function GradientIcon() {
-    return (
-        <span className="h-6 w-6 rounded-full bg-gradient-to-tr from-[#096cde] via-[#096cde] via-30% to-[#ddf1ff]" />
-    );
+  return (
+    <span className="h-6 w-6 rounded-full bg-gradient-to-tr from-[#096bde] via-[#096bde] via-30% to-[#ddf1ff]" />
+  );
 }
+
 export default GradientIcon;
