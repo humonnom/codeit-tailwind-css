5a0063d4f18447db6234ef7ce41499d59a2907e1
humonnom <juepark42@gmail.com>
2024-09-22 00:37:57 +0900

    skeleton

--- src/components/GradientIcon.tsx
+++ src/components/GradientIcon.tsx
@@ -1,6 +1,5 @@
 function GradientIcon() {
-    return (
-        <span className="h-6 w-6 rounded-full bg-gradient-to-tr from-[#096cde] via-[#096cde] via-30% to-[#ddf1ff]" />
-    );
+  return <span className="" />;
 }
+
 export default GradientIcon;
