cc86d104226397b948b7e782c265313a1c2859d5
humonnom <juepark42@gmail.com>
2024-09-22 00:03:28 +0900

    fix motion reduce class

--- src/components/AnimatedSlideAnchor.tsx
+++ src/components/AnimatedSlideAnchor.tsx
@@ -9,21 +9,21 @@ function AnimatedSlideAnchor() {
           viewBox="0 0 20 20"
           aria-hidden="true"
           focusable="false"
-          className="reduced-motion:group-hover:-translate-x-full h-6 w-6 -translate-x-full opacity-0 transition-all duration-500 will-change-transform group-hover:translate-x-0 group-hover:opacity-100"
+          className="motion-reduce:group-hover:-translate-x-full h-6 w-6 -translate-x-full opacity-0 transition-all duration-500 will-change-transform group-hover:translate-x-0 group-hover:opacity-100"
         >
           <path
             d="M17.707 9.293l-5-5a.999.999 0 10-1.414 1.414L14.586 9H3a1 1 0 100 2h11.586l-3.293 3.293a.999.999 0 101.414 1.414l5-5a.999.999 0 000-1.414z"
             fill="currentColor"
           ></path>
         </svg>
-        <span className="reduced-motion:group-hover:translate-x-0 -translate-x-5 transition-transform duration-500 will-change-transform group-hover:translate-x-2">
+        <span className="motion-reduce:group-hover:-translate-x-5 -translate-x-5 transition-transform duration-500 will-change-transform group-hover:translate-x-2">
           Start selling
         </span>
         <svg
           viewBox="0 0 20 20"
           aria-hidden="true"
           focusable="false"
-          className="reduced-motion:group-hover:translate-x-0 ml-auto h-6 w-6 translate-x-0 opacity-100 transition-all duration-500 will-change-transform group-hover:translate-x-full group-hover:opacity-0"
+          className="motion-reduce:group-hover:translate-x-0 ml-auto h-6 w-6 translate-x-0 opacity-100 transition-all duration-500 will-change-transform group-hover:translate-x-full group-hover:opacity-0"
         >
           <path
             d="M17.707 9.293l-5-5a.999.999 0 10-1.414 1.414L14.586 9H3a1 1 0 100 2h11.586l-3.293 3.293a.999.999 0 101.414 1.414l5-5a.999.999 0 000-1.414z"
