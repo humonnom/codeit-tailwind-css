f3edc03bee914e9907fdfee0715eec61f52a55d1
humonnom <juepark42@gmail.com>
2024-09-28 17:22:26 +0900

    delete unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import usePrevious from "@/hooks/usePrevious";
 import { Button } from "@nextui-org/react";
 
