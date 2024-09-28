166ea9388aedc25e15c77edb7a64b8e305316479
humonnom <juepark42@gmail.com>
2024-09-28 21:33:47 +0900

    draw black circle

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -5,7 +5,11 @@ type DoughnutChartProps = {
 };
 
 const DonutChart = ({}: DoughnutChartProps) => {
-  return <></>;
+  return (
+    <svg width={100} height={100} viewBox={"0 0 100 100"}>
+      <circle cx="50" cy="50" r="50" />
+    </svg>
+  );
 };
 
 export default DonutChart;
