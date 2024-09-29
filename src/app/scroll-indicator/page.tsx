0c3c6578674e891dcba8366d3c60967db6d77277
humonnom <juepark42@gmail.com>
2024-09-29 20:26:27 +0900

    scroll indicator 완성

---
+++ src/app/scroll-indicator/page.tsx
@@ -0,0 +1,102 @@
+"use client";
+import { useScroll, animated } from "@react-spring/web";
+
+function LoremIpsum() {
+  return (
+    <div>
+      <article className={"max-w-[500px] p-5 mx-auto my-0"}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac
+          rhoncus quam.
+        </p>
+        <p>
+          Fringilla quam urna. Cras turpis elit, euismod eget ligula quis,
+          imperdiet sagittis justo. In viverra fermentum ex ac vestibulum.
+          Aliquam eleifend nunc a luctus porta. Mauris laoreet augue ut felis
+          blandit, at iaculis odio ultrices. Nulla facilisi. Vestibulum cursus
+          ipsum tellus, eu tincidunt neque tincidunt a.
+        </p>
+        <h2>Sub-header</h2>
+        <p>
+          In eget sodales arcu, consectetur efficitur metus. Duis efficitur
+          tincidunt odio, sit amet laoreet massa fringilla eu.
+        </p>
+        <p>
+          Pellentesque id lacus pulvinar elit pulvinar pretium ac non urna.
+          Mauris id mauris vel arcu commodo venenatis. Aliquam eu risus arcu.
+          Proin sit amet lacus mollis, semper massa ut, rutrum mi.
+        </p>
+        <p>Sed sem nisi, luctus consequat ligula in, congue sodales nisl.</p>
+        <p>
+          Vestibulum bibendum at erat sit amet pulvinar. Pellentesque pharetra
+          leo vitae tristique rutrum. Donec ut volutpat ante, ut suscipit leo.
+        </p>
+        <h2>Sub-header</h2>
+        <p>
+          Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum fringilla
+          aliquet. Pellentesque auctor vehicula malesuada. Aliquam id feugiat
+          sem, sit amet tempor nulla. Quisque fermentum felis faucibus, vehicula
+          metus ac, interdum nibh. Curabitur vitae convallis ligula. Integer ac
+          enim vel felis pharetra laoreet. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Pellentesque hendrerit ac augue quis
+          pretium.
+        </p>
+        <p>
+          Morbi ut scelerisque nibh. Integer auctor, massa non dictum tristique,
+          elit metus efficitur elit, ac pretium sapien nisl nec ante. In et ex
+          ultricies, mollis mi in, euismod dolor.
+        </p>
+        <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
+        <p>
+          Pellentesque id lacus pulvinar elit pulvinar pretium ac non urna.
+          Mauris id mauris vel arcu commodo venenatis. Aliquam eu risus arcu.
+          Proin sit amet lacus mollis, semper massa ut, rutrum mi.
+        </p>
+        <p>Sed sem nisi, luctus consequat ligula in, congue sodales nisl.</p>
+        <p>
+          Vestibulum bibendum at erat sit amet pulvinar. Pellentesque pharetra
+          leo vitae tristique rutrum. Donec ut volutpat ante, ut suscipit leo.
+        </p>
+        <h2>Sub-header</h2>
+        <p>
+          Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum fringilla
+          aliquet. Pellentesque auctor vehicula malesuada. Aliquam id feugiat
+          sem, sit amet tempor nulla. Quisque fermentum felis faucibus, vehicula
+          metus ac, interdum nibh. Curabitur vitae convallis ligula. Integer ac
+          enim vel felis pharetra laoreet. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Pellentesque hendrerit ac augue quis
+          pretium.
+        </p>
+        <p>
+          Morbi ut scelerisque nibh. Integer auctor, massa non dictum tristique,
+          elit metus efficitur elit, ac pretium sapien nisl nec ante. In et ex
+          ultricies, mollis mi in, euismod dolor.
+        </p>
+        <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
+      </article>
+    </div>
+  );
+}
+
+function ScrollIndicator() {
+  const { scrollYProgress } = useScroll({
+    config: { duration: 20, easing: (num) => num },
+  });
+
+  return (
+    <div className={"scroll-indicator"}>
+      <animated.div
+        className="fixed top-0 left-0 right-0 h-2.5 bg-red-500 origin-left"
+        style={{ scaleX: scrollYProgress }}
+      />
+      <h1
+        className={"font-bold text-4xl text-center mt-[100px] mx-0 mb-[40px]"}
+      >
+        <code>useScroll</code> demo
+      </h1>
+      <LoremIpsum />
+    </div>
+  );
+}
+
+export default ScrollIndicator;
