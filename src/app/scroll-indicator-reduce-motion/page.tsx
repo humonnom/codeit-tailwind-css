19754ef3d651f061b55df0ca46cd2771bb6ffffc
humonnom <juepark42@gmail.com>
2024-09-29 20:58:11 +0900

    scroll indicator reduce motion 완료

---
+++ src/app/scroll-indicator-reduce-motion/page.tsx
@@ -0,0 +1,116 @@
+"use client";
+import React from "react";
+import { useScroll, animated, useReducedMotion } from "@react-spring/web";
+
+const ScrollIndicator = () => {
+  const { scrollYProgress } = useScroll();
+
+  // 대안 : tailwind motion-reduce: 대신에 reduceMotion을 flag로 사용할 수 있습니다.
+  // const reduceMotion = useReducedMotion();
+
+  return (
+    <div>
+      <div
+        className={
+          "top-0 right-0 p-3 motion-reduce:flex hidden fixed items-center gap-1"
+        }
+      >
+        <animated.span className="font-bold text-xl text-gray-800">
+          {scrollYProgress.to((value) => `${Math.round(value * 100)}`)}
+        </animated.span>
+        <span className={"text-sm text-gray-500"}>%</span>
+      </div>
+      <animated.div
+        className="fixed top-0 left-0 right-0 h-2.5 bg-red-500 origin-left motion-reduce:hidden"
+        style={{
+          scaleX: scrollYProgress,
+        }}
+      />
+      <h1
+        className={"font-bold text-4xl text-center mt-[100px] mx-0 mb-[40px]"}
+      >
+        <code>useScroll</code> demo
+      </h1>
+      <LoremIpsum />
+    </div>
+  );
+};
+
+function LoremIpsum() {
+  return (
+    <div>
+      <article className={"max-w-[500px] p-5 mx-auto my-0"}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac
+          rhoncus quam.
+        </p>
+        <p>
+          Fringilla quam urna. Cras turpis elit, euismod eget ligula quis,
+          imperdiet sagittis justo. In viverra fermentum ex ac vestibulum.
+          Aliquam eleifend nunc a luctus porta. Mauris laoreet augue ut felis
+          blandit, at iaculis odio ultrices. Nulla facilisi. Vestibulum cursus
+          ipsum tellus, eu tincidunt neque tincidunt a.
+        </p>
+        <h2>Sub-header</h2>
+        <p>
+          In eget sodales arcu, consectetur efficitur metus. Duis efficitur
+          tincidunt odio, sit amet laoreet massa fringilla eu.
+        </p>
+        <p>
+          Pellentesque id lacus pulvinar elit pulvinar pretium ac non urna.
+          Mauris id mauris vel arcu commodo venenatis. Aliquam eu risus arcu.
+          Proin sit amet lacus mollis, semper massa ut, rutrum mi.
+        </p>
+        <p>Sed sem nisi, luctus consequat ligula in, congue sodales nisl.</p>
+        <p>
+          Vestibulum bibendum at erat sit amet pulvinar. Pellentesque pharetra
+          leo vitae tristique rutrum. Donec ut volutpat ante, ut suscipit leo.
+        </p>
+        <h2>Sub-header</h2>
+        <p>
+          Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum fringilla
+          aliquet. Pellentesque auctor vehicula malesuada. Aliquam id feugiat
+          sem, sit amet tempor nulla. Quisque fermentum felis faucibus, vehicula
+          metus ac, interdum nibh. Curabitur vitae convallis ligula. Integer ac
+          enim vel felis pharetra laoreet. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Pellentesque hendrerit ac augue quis
+          pretium.
+        </p>
+        <p>
+          Morbi ut scelerisque nibh. Integer auctor, massa non dictum tristique,
+          elit metus efficitur elit, ac pretium sapien nisl nec ante. In et ex
+          ultricies, mollis mi in, euismod dolor.
+        </p>
+        <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
+        <p>
+          Pellentesque id lacus pulvinar elit pulvinar pretium ac non urna.
+          Mauris id mauris vel arcu commodo venenatis. Aliquam eu risus arcu.
+          Proin sit amet lacus mollis, semper massa ut, rutrum mi.
+        </p>
+        <p>Sed sem nisi, luctus consequat ligula in, congue sodales nisl.</p>
+        <p>
+          Vestibulum bibendum at erat sit amet pulvinar. Pellentesque pharetra
+          leo vitae tristique rutrum. Donec ut volutpat ante, ut suscipit leo.
+        </p>
+        <h2>Sub-header</h2>
+        <p>
+          Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum fringilla
+          aliquet. Pellentesque auctor vehicula malesuada. Aliquam id feugiat
+          sem, sit amet tempor nulla. Quisque fermentum felis faucibus, vehicula
+          metus ac, interdum nibh. Curabitur vitae convallis ligula. Integer ac
+          enim vel felis pharetra laoreet. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Pellentesque hendrerit ac augue quis
+          pretium.
+        </p>
+        <p>
+          Morbi ut scelerisque nibh. Integer auctor, massa non dictum tristique,
+          elit metus efficitur elit, ac pretium sapien nisl nec ante. In et ex
+          ultricies, mollis mi in, euismod dolor.
+        </p>
+        <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
+      </article>
+    </div>
+  );
+}
+
+export default ScrollIndicator;
